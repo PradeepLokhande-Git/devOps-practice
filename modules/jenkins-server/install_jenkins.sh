6789acd3647bb5df1bb622298e47b63d1ed07c0b
Pradeep Lokhande <pradeep.lokhande.gslab@sentilink.com>
2024-05-04 04:09:42 -0700

    ch

---
+++ modules/jenkins-server/install_jenkins.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+
+#Install java 11 and maven -
+yum update -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install -y maven
+
+#Install Jenkins
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
+
+#Install Docker 
+yum install -y docker
+systemctl start docker
+yum install -y git
+yum install -y pip
+yum install -y tree
+
+usermod -a -G docker jenkins
+
+systemctl restart jenkins
+systemctl daemon-reload
+systemctl stop docker 
+
+#Install Terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+#Install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
---
+++ modules/jenkins-server/main.tf
@@ -0,0 +1,117 @@
+#
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.10.0"
+    }
+  }
+}
+#Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#Create EC2 Instance
+resource "aws_instance" "jenkins-ec2" {
+  ami           = "ami-07caf09b362be10b8"
+  instance_type = "t2.medium"
+  key_name      = "pradeep-devops-account"
+  user_data     = file("install_jenkins.sh")
+  vpc_security_group_ids = [aws_security_group.myjenkins_sg.id]
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+/*
+resource "aws_ebs_volume" "ebs-vol" {
+  availability_zone = aws_instance.jenkins-ec2.availability_zone
+  size              = 20
+  tags = {
+    Name = "Jenkins_ebs_volume"
+  }
+}
+resource "aws_volume_attachment" "ebs-attach" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.ebs-vol.id
+  instance_id = aws_instance.jenkins-ec2.id
+}
+
+output "instance_id" {
+  value = aws_instance.jenkins-ec2.id
+}
+
+output "volume_id" {
+  value = aws_ebs_volume.ebs-vol.id
+}
+
+output "attachment_id" {
+  value = aws_volume_attachment.ebs-attach.id
+}
+*/
+#Create security group 
+resource "aws_security_group" "myjenkins_sg" {
+  name        = "pradeep-jenkins_sg"
+  description = "Allow inbound ports 22, 8080"
+  vpc_id      = "vpc-091d1aeeaf776755e"
+ tags = {
+    Name = "pradeep-jenkins_sg"
+  }
+  #Allow incoming TCP requests on port 22 from any IP
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["104.30.160.2/32"]
+  }
+  #Allow incoming TCP requests on port 443 from any IP
+  ingress {
+    description = "Allow HTTPS Traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["104.30.160.2/32"]
+  }
+
+  #Allow incoming TCP requests on port 8080 from any IP
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["104.30.160.2/32"]
+  }
+
+  #Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+/*
+#Create S3 bucket for Jenksin Artifacts
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket = "pradeep-jenkins-s3-bucket-terraform"
+
+  tags = {
+    Name = "pradeep-Jenkins-Server"
+  }
+}
+
+#make sure is prive and not open to public and create Access control List
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket     = aws_s3_bucket.my-s3-bucket.id
+  acl        = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+*/
