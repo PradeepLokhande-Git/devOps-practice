918f83c440610a1aa5f882bcbb5414355401a571
Pradeep Lokhande <pradeep.lokhande.gslab@sentilink.com>
2024-05-04 02:36:28 -0700

    added modules

---
+++ modules/k8s-cluster/eks.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "eks_demo" {
+  name               = "eks-demo-cluster"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-demo-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_demo.name
+}
+
+resource "aws_eks_cluster" "eks-demo" {
+  name     = "eks-demo-cluster"
+  role_arn = aws_iam_role.eks_demo.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.private-us-east-1a.id,
+      aws_subnet.private-us-east-1b.id,
+      aws_subnet.public-us-east-1a.id,
+      aws_subnet.public-us-east-1b.id
+    ]
+  }
+  depends_on = [aws_iam_role_policy_attachment.eks-demo-AmazonEKSClusterPolicy]
+}
+
